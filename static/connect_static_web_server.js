80892a403b6a206bca2564674b646eb6a9108dc7
tangxinfa <tangxinfa@gmail.com>
2013-07-17 00:06:40 +0800

    write slide 《理解Node.js》

--- static/connect_static_web_server.js
+++ static/connect_static_web_server.js
@@ -1,6 +1,6 @@
 var connect = require('connect');
 var app = connect()
-    .use(connect.logger())               // 输出访问
+    .use(connect.logger())               // 访问日志
     .use(connect.static(__dirname))      // 静态文件
     .use(connect.directory(__dirname))   // 目录浏览
     .listen(8004);
