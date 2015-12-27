5294a1ad3a4c3c0c023346b369a002bf1151631a
tangxinfa <tangxinfa@gmail.com>
2015-12-27 17:45:14 +0800

    Add missing js functions: CodeHighlightOn and CodeHighlightOff.

--- style/js/o-blog-fix.js
+++ style/js/o-blog-fix.js
@@ -65,3 +65,20 @@ $(document).ready(
 
     }
 )
+
+function CodeHighlightOn(elem, id)  {
+    var target = document.getElementById(id);
+    if(null != target) {
+        elem.cacheClassElem = elem.className;
+        elem.cacheClassTarget = target.className;
+        target.className = "code-highlighted";
+        elem.className   = "code-highlighted";
+    }
+}
+function CodeHighlightOff(elem, id) {
+    var target = document.getElementById(id);
+    if(elem.cacheClassElem)
+        elem.className = elem.cacheClassElem;
+    if(elem.cacheClassTarget)
+        target.className = elem.cacheClassTarget;
+}
