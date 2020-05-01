82926c4d7fb3a6995c8bdcd17e1cc29b75976d69
tangxinfa <tangxinfa@gmail.com>
2020-05-01 10:10:28 +0800

    fix: fullscreen

--- static/memorizing-english-words/player.js
+++ static/memorizing-english-words/player.js
@@ -19,7 +19,7 @@ function next_word() {
 
 function player_start(name) {
   player_dictionary = dictionaries[name];
-  if (typeof player_dictionary === "object" ) {
+  if (typeof player_dictionary === "object") {
     if (player_timer) {
       clearInterval(player_timer);
     }
@@ -44,6 +44,53 @@ function player_dictionary_change() {
   player_prepare($("#dictionary").val());
 }
 
+function player_fullscreening() {
+  return (
+    document.fullscreenElement ||
+    document.mozFullScreenElement ||
+    document.webkitFullscreenElement ||
+    document.msFullscreenElement
+  );
+}
+
+function player_fullscreen_on() {
+  if (!player_fullscreening()) {
+    if (document.documentElement.requestFullscreen) {
+      document.documentElement.requestFullscreen();
+    } else if (document.documentElement.msRequestFullscreen) {
+      document.documentElement.msRequestFullscreen();
+    } else if (document.documentElement.mozRequestFullScreen) {
+      document.documentElement.mozRequestFullScreen();
+    } else if (document.documentElement.webkitRequestFullscreen) {
+      document.documentElement.webkitRequestFullscreen(
+        Element.ALLOW_KEYBOARD_INPUT
+      );
+    }
+  }
+}
+
+function player_fullscreen_off() {
+  if (player_fullscreening()) {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.msExitFullscreen) {
+      document.msExitFullscreen();
+    } else if (document.mozCancelFullScreen) {
+      document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen();
+    }
+  }
+}
+
+function player_fullscreen_toggle() {
+  if (player_fullscreening()) {
+    player_fullscreen_off();
+  } else {
+    player_fullscreen_on();
+  }
+}
+
 function player_init() {
   $("#dictionary").change(player_dictionary_change);
   for (let name in dictionaries) {
@@ -55,6 +102,9 @@ function player_init() {
     }
   }
   player_dictionary_change();
+  $("body").on("touchend", function() {
+    player_fullscreen_toggle();
+  });
 }
 
 $(document).ready(player_init);
