438297f957df8d64287d4bcdb41e3ac1d930eaca
tangxinfa <tangxinfa@gmail.com>
2020-05-06 12:58:39 +0800

    fix: add new dictionary

--- static/memorizing-english-words/index.js
+++ static/memorizing-english-words/index.js
@@ -1,1 +1,1 @@
-var dictionaries = {"GRE":"GRE.js","TOEFL":"TOEFL.js","NPEE":"NPEE.js","COCA":"COCA.js","CET6":"CET6.js","CET4":"CET4.js"};
+var dictionaries = {"GRE":"GRE.js","TOEFL":"TOEFL.js","OALD":"OALD.js","NPEE":"NPEE.js","COCA":"COCA.js","CET6":"CET6.js","CET4":"CET4.js"};
