d0255f5db0b65c13582c8406afc51f8b378d7cad
tangxinfa <tangxinfa@gmail.com>
2020-05-12 12:19:47 +0800

    fix: CET6 dictionary.

--- static/memorizing-english-words/index.js
+++ static/memorizing-english-words/index.js
@@ -1,1 +1,1 @@
-var dictionaries = {"GRE":"GRE.js","TOEFL":"TOEFL.js","OALD":"OALD.js","NPEE":"NPEE.js","COCA":"COCA.js","CET6":"CET6.js","CET4":"CET4.js"};
+var dictionaries = {"CET6":"CET6.js","GRE":"GRE.js","TOEFL":"TOEFL.js","OALD":"OALD.js","NPEE":"NPEE.js","COCA":"COCA.js","CET4":"CET4.js"};
